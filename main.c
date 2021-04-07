84b8a18b98d70463c2ec12ead532f6e188053b8e
André Cardoso <andre.cardoso@lsdi.ufma.br>
2021-04-07 18:48:59 -0300

    Commit Inicial

--- main.c
+++ main.c
@@ -5,20 +5,57 @@
 #include <unistd.h>
 
 // Constantes
-#define LINES 12
-#define COLUMNS 12
-#define NUMBER_OF_THREADS 6
+#define LINES 4000
+#define COLUMNS 4000
+#define NUMBER_OF_THREADS 2
 #define divisor_linhas  2
-#define divisor_colunas  3
+#define divisor_colunas  2
 
-// Indexes
+// Peterson's Solution
 
+#define FALSE 0
+#define TRUE 0
+#define N NUMBER_OF_THREADS
+
+
+int turn;
+int interested[N];
+
+void init_interested_vector(){
+    int i;
+    for (i = 0; i < N; i++){
+        interested[i] = 0;
+    }
+
+}
+
+void enter_region(int process){
+
+    //flag[self]=1 show that process self want to enter in critical section
+    if(process < N-1 && process >=0){
+        interested[process] = 1;
+    }
+    turn = N-process;
+    if(N-1-process != process){
+        while(interested[N-1-process]==1 && turn==N-process);
+
+    }else if (N-2-process >= 0){
+        while(interested[N-2-process]==1 && turn==N-process);
+    }
+
+}
+
+void leave_region(int process){
+    interested[process] = FALSE;
+    if(process <= N && process >= 0){
+        interested[process] = 0;
+    }
+}
 
 
 // Declaração de Variáveis Globais
 
 int matrix[LINES][COLUMNS];
-
 int contador_sequencial = 0;
 int contador_paralelo = 0;
 
@@ -26,12 +63,12 @@ int contador_paralelo = 0;
 // Declaração de Métodos
 int isNumberPrime(int number);
 void createMatrix();
-void defineSubMatrix();
-void teste();
 void contagemSequencial();
-void contagemParalela(void *ptr);
+void *contagemParalela(void *ptr);
 int isMatrixDivisibleByNumberOfThreads();
-void *_thread_loop(void *p);
+pthread_mutex_t lock;
+
+
 struct subMatriz
 {
     int i_start_index;
@@ -43,94 +80,130 @@ struct subMatriz
 
 struct subMatriz *subMatrizes;
 
-void printHelloWorld(void *ptr){
+void contagemSequencial(){
+    int i,j,number;
+
+    for(i=0; i<LINES; i++) {
+        for(j=0;j<COLUMNS;j++) {
+
+            number = matrix[i][j];
+
+            if(isNumberPrime(number)){
+                contador_sequencial++;
+            }
+
+        }
+    }
+}
+void *contagemParalela(void *ptr){
 
     int contador_local = 0;
-    int n = (int)ptr;
+    int n = (intptr_t) ptr;
 
 
     int i,j;
 
     for(i = subMatrizes[n].i_start_index; i < subMatrizes[n].i_stop_index; i++ ){
-        printf("\n");
 
         for(j = subMatrizes[n].j_start_index; j < subMatrizes[n].j_stop_index; j++ ){
 
             if(isNumberPrime(matrix[i][j])){
                 contador_local++;
+
+
+
             }
         }
     }
-    printf("\nThread %d: %d números primos encontrados",n, contador_local);
+//    printf("\nContador Local da Thread %d encontrou %d números primos",n, contador_local);
+
+    enter_region(n);
+    printf("\nThread %d incrementando contador global %d vezes",n, contador_local);
+    for(i = 0; i<contador_local; i++){
+        contador_paralelo++;
+    }
+    printf("\nThread %d liberando o contador global!",n);
+    leave_region(n);
+
     pthread_exit(NULL);
 
 
 }
 
-
-
 int isMatrixDivisibleByNumberOfThreads(){
     div_t verifica_linhas, verifica_colunas;
     verifica_linhas = div(LINES, divisor_linhas);
     verifica_colunas = div(COLUMNS, divisor_colunas);
-    printf("\n  Verificando se a matrix (%d x %d) pode ser dividida em %d partes iguais",LINES, COLUMNS, NUMBER_OF_THREADS);
+    printf("\nVerificando se a matrix (%d x %d) pode ser dividida em %d partes iguais",LINES, COLUMNS, NUMBER_OF_THREADS);
 
-    printf("\n      Linhas: resto da divisão entre %d e %d = %d", LINES, divisor_linhas, verifica_linhas.rem);
-    printf("\n      Colunas: resto da divisão entre %d e %d = %d", COLUMNS, divisor_colunas, verifica_colunas.rem);
+    printf("\n  %d %% %d = %d", LINES, divisor_linhas, verifica_linhas.rem);
+    printf("\n  %d %% %d = %d", COLUMNS, divisor_colunas, verifica_colunas.rem);
 
 
     return (verifica_linhas.rem == 0 && verifica_colunas.rem == 0);
 
 }
+void debugMatrix(){
+    int i,j;
+
+    createMatrix();
+    for(i=0; i<LINES; i++) {
+        printf("\n", matrix[i][j]);
+
+        for(j=0;j<COLUMNS;j++) {
+
+            printf(" %d", matrix[i][j]);
 
 
 
-int main(int argc, char **argv) {
+        }
+    }
+}
+
+
+
+int main() {
 
 
     printf("\n=============== Iniciando o Experimento ===============");
     printf("\nTamanho da matrix: (%d x %d);", LINES, COLUMNS);
-//    todo() verificar se o lines + columns = numero de threads
+    printf("\nNúmero de threads: %d;", NUMBER_OF_THREADS);
+
+    if(NUMBER_OF_THREADS != divisor_colunas * divisor_linhas){
+        printf("\nDefina corretamente o número de threads e os numeros divisores, a seguinte condição deve ser satisfeita: NUMBER_OF_THREADS == divisor_linhas * divisor colunas");
+        printf("\n----------------------- Finalizando Experimento -------------------------");
+        return 0;
+    }
+
+    if (!isMatrixDivisibleByNumberOfThreads()){
+        printf("\nA Matrix (%d x %d) não pode ser dividida igualmente em %d partes.", LINES, COLUMNS, NUMBER_OF_THREADS);
+        printf("\n----------------------- Finalizando Experimento -------------------------");
+
+        return 0;
+    }else{
+        printf("\nA Matrix (%d x %d) pode ser dividida igualmente em %d partes.", LINES, COLUMNS, NUMBER_OF_THREADS);
+
+    }
+
+
+    init_interested_vector();
     createMatrix();
+    printf("\nMatriz Criada!");
     printf("\n ----------------------- Iniciando Contagem Sequencial -------------------------");
     contagemSequencial();
-    printf("\nResultado: %d números primos encontrados.", contador_sequencial);
     printf("\n ----------------------- Finalizando Contagem Sequencial -------------------------\n");
 
 
 
     printf("\n----------------------- Iniciando Contagem Paralela -------------------------");
 
-    printf("\nNúmero de threads: %d;\n", NUMBER_OF_THREADS);
-
-    if (!isMatrixDivisibleByNumberOfThreads()){
-        printf("\n  A Matrix (%d x %d) não pode ser dividida igualmente em %d partes.", LINES, COLUMNS, NUMBER_OF_THREADS);
-        printf("\n----------------------- Finalizando Contagem Paralela -------------------------");
-
-        return 0;
-    }else{
-        printf("\n  A Matrix (%d x %d) pode ser dividida igualmente em %d partes.", LINES, COLUMNS, NUMBER_OF_THREADS);
-        printf("\n  Iniciando Divisão da Matrix");
-    }
 
 
+
+    printf("\nIniciando Divisão da Matrix");
     int bloco_linhas = LINES/ divisor_linhas;
     int bloco_colunas = COLUMNS/ divisor_colunas;
 
-//    remover
-//    printf("\ndivisor linha: %d e bloco coliuna: %d -> ", bloco_linhas, bloco_colunas);
-
-
-//    remover
-//    printf("\nObjetivo:\n");
-//    printf("\nBloco 1 -> linhas: 0 a 10500; coluna: 0 a 7000");
-//    printf("\nBloco 2 -> linhas: 0 a 10500; coluna: 7000 a 14000");
-//    printf("\nBloco 3 -> linhas: 0 a 10500; coluna: 14000 a 21000");
-//
-//    printf("\nBloco 4 -> linhas: 10500 a 21000; coluna: 0 a 7000");
-//    printf("\nBloco 5 -> linhas: 10500 a 21000; coluna: 7000 a 14000");
-//    printf("\nBloco 6 -> linhas: 10500 a 21000; coluna: 14000 a 21000");
-//    printf("\nObjetivo:\n");
 
     subMatrizes = (struct subMatriz*)malloc(NUMBER_OF_THREADS*sizeof(struct subMatriz));
 
@@ -141,8 +214,6 @@ int main(int argc, char **argv) {
     for (i=0; i < divisor_linhas; i++){
         aux1 = 1;
         for(j=0; j < divisor_colunas; j++){
-            printf("\nSubmatriz %d : linhas: %d a %d, colunas: %d a %d ",counter, i*bloco_linhas, aux2* bloco_linhas, j* bloco_colunas, aux1* bloco_colunas);
-
             subMatrizes[counter].i_start_index = i * bloco_linhas;
             subMatrizes[counter].i_stop_index  = aux2 * bloco_linhas;
             subMatrizes[counter].j_start_index = j * bloco_colunas;
@@ -154,70 +225,34 @@ int main(int argc, char **argv) {
 
     }
 
-//    for (int p=0; p<NUMBER_OF_THREADS; p++){ DEBUG
-//        printf("\n----");
-//        printf("\ni start = %d", subMatrizes[p].i_start_index);
-//        printf("\ni stop = %d", subMatrizes[p].i_stop_index );
-//        printf("\nj start = %d", subMatrizes[p].j_start_index);
-//        printf("\nj stop = %d",subMatrizes[p].j_stop_index );
-//        printf("\n----");
-//    }
-
-//    printf("\n------------------\n"); DEBUG
-//    int k,y;
-//    for(k=0; k<LINES; k++) {
-//        printf("\n");
-//        for(y=0;y<COLUMNS;y++) {
-//            printf("%d ", matrix[k][y]);
-//        }
-//        printf("\n");
-//    }
-//    printf("\n------------------\n");
-
-
 
     pthread_t threads[NUMBER_OF_THREADS];
 
     int  status;
-//    status = pthread_create( &threads[0], NULL, printHelloWorld, (void *) 0);
-//    if(status != 0){
-//        printf("error");
-//    }
-//    pthread_join(threads[0], NULL);
-
 
     for (i=0; i < NUMBER_OF_THREADS; i++){
 
-        status = pthread_create( &threads[i], NULL, printHelloWorld, (void *) i);
+        status = pthread_create( &threads[i], NULL, contagemParalela, (void *)(intptr_t)i);
         if(status != 0){
             printf("error");
         }
     }
-//
     for (i=0; i < NUMBER_OF_THREADS; i++){
 
         pthread_join(threads[i], NULL);
 
     }
-//
-//    sleep(5);
+    printf("\n----------------------- Finalizando Contagem Paralela -------------------------");
 
-
-//    int i,j;
-//    createMatrix();
-
-//    i = 10;
-    #define x i
-
-
-//    if(result.rem == 0){
-//        printf("Divisão inteira %d", x);
-//
-//
-//    }
-
-
-//
+    if(contador_paralelo != contador_sequencial){
+        printf("Erro na contagem!");
+        printf("\n\nContador Sequencial --> %d números primos", contador_sequencial);
+        printf("\nContador Global --> %d números primos", contador_paralelo);
+    }else{
+        printf("\nResultado:");
+        printf("\nContador Sequencial --> %d números primos", contador_sequencial);
+        printf("\nContador Global --> %d números primos", contador_paralelo);
+    }
 
 
     return 0;
@@ -226,8 +261,6 @@ int main(int argc, char **argv) {
 void createMatrix(){
     int a;
     int i,j;
-
-
     for(i=0; i<LINES; i++) {
         for(j=0;j<COLUMNS;j++) {
             a = rand() % 29999;
@@ -259,48 +292,7 @@ int isNumberPrime(int number){
     }
 }
 
-void contagemSequencial(){
-    int i,j,number;
-
-    for(i=0; i<LINES; i++) {
-        for(j=0;j<COLUMNS;j++) {
-
-            number = matrix[i][j];
-
-            if(isNumberPrime(number)){
-                contador_sequencial++;
-            }
-
-        }
-    }
-}
-
-void *_thread_loop(void *p)
-{
-    printf("hello thread");
-    int contador_local = 0, number;
-    uint64_t n = (uint64_t)p;
-
-    int i, j;
-    for(i = subMatrizes[n].i_start_index; i<subMatrizes[n].i_stop_index; i++ ) {
-        for (j = subMatrizes[n].j_start_index; i < subMatrizes[n].j_stop_index; j++) {
-            number = matrix[i][j];
-            if (isNumberPrime(number)) {
-                contador_local++;
-            }
-
-        }
-
-    }
-    return NULL;
-}
-
-void contagemParalela(void *ptr){
 
 
-}
 
-void teste(){
-    printf("%d %d ", LINES, COLUMNS);
-}
 
